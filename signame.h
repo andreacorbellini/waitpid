0d9a4af8f452e9275e0b9740e26b581d20297eeb
Andrea Corbellini <corbellini.andrea@gmail.com>
2014-03-08 18:01:18 +0100

    Adapt signame.h to the new coding style.

--- signame.h
+++ signame.h
@@ -1,111 +1,113 @@
 static const char *
-signame (int signal) {
-  switch (signal) {
+signame (int signal)
+{
+  switch (signal)
+    {
 #ifdef SIGHUP
-    case SIGHUP: return "SIGHUP";
+      case SIGHUP: return "SIGHUP";
 #endif
 #ifdef SIGINT
-    case SIGINT: return "SIGINT";
+      case SIGINT: return "SIGINT";
 #endif
 #ifdef SIGQUIT
-    case SIGQUIT: return "SIGQUIT";
+      case SIGQUIT: return "SIGQUIT";
 #endif
 #ifdef SIGILL
-    case SIGILL: return "SIGILL";
+      case SIGILL: return "SIGILL";
 #endif
 #ifdef SIGTRAP
-    case SIGTRAP: return "SIGTRAP";
+      case SIGTRAP: return "SIGTRAP";
 #endif
 #ifdef SIGABRT
-    case SIGABRT: return "SIGABRT";
+      case SIGABRT: return "SIGABRT";
 #endif
 #ifdef SIGFPE
-    case SIGFPE: return "SIGFPE";
+      case SIGFPE: return "SIGFPE";
 #endif
 #ifdef SIGKILL
-    case SIGKILL: return "SIGKILL";
+      case SIGKILL: return "SIGKILL";
 #endif
 #ifdef SIGBUS
-    case SIGBUS: return "SIGBUS";
+      case SIGBUS: return "SIGBUS";
 #endif
 #ifdef SIGSEGV
-    case SIGSEGV: return "SIGSEGV";
+      case SIGSEGV: return "SIGSEGV";
 #endif
 #ifdef SIGPIPE
-    case SIGPIPE: return "SIGPIPE";
+      case SIGPIPE: return "SIGPIPE";
 #endif
 #ifdef SIGALRM
-    case SIGALRM: return "SIGALRM";
+      case SIGALRM: return "SIGALRM";
 #endif
 #ifdef SIGTERM
-    case SIGTERM: return "SIGTERM";
+      case SIGTERM: return "SIGTERM";
 #endif
 #ifdef SIGURG
-    case SIGURG: return "SIGURG";
+      case SIGURG: return "SIGURG";
 #endif
 #ifdef SIGSTOP
-    case SIGSTOP: return "SIGSTOP";
+      case SIGSTOP: return "SIGSTOP";
 #endif
 #ifdef SIGTSTP
-    case SIGTSTP: return "SIGTSTP";
+      case SIGTSTP: return "SIGTSTP";
 #endif
 #ifdef SIGCONT
-    case SIGCONT: return "SIGCONT";
+      case SIGCONT: return "SIGCONT";
 #endif
 #ifdef SIGCHLD
-    case SIGCHLD: return "SIGCHLD";
+      case SIGCHLD: return "SIGCHLD";
 #endif
 #ifdef SIGTTIN
-    case SIGTTIN: return "SIGTTIN";
+      case SIGTTIN: return "SIGTTIN";
 #endif
 #ifdef SIGTTOU
-    case SIGTTOU: return "SIGTTOU";
+      case SIGTTOU: return "SIGTTOU";
 #endif
 #ifdef SIGIO
-    case SIGIO: return "SIGIO";
+      case SIGIO: return "SIGIO";
 #endif
 #ifdef SIGXCPU
-    case SIGXCPU: return "SIGXCPU";
+      case SIGXCPU: return "SIGXCPU";
 #endif
 #ifdef SIGXFSZ
-    case SIGXFSZ: return "SIGXFSZ";
+      case SIGXFSZ: return "SIGXFSZ";
 #endif
 #ifdef SIGVTALRM
-    case SIGVTALRM: return "SIGVTALRM";
+      case SIGVTALRM: return "SIGVTALRM";
 #endif
 #ifdef SIGPROF
-    case SIGPROF: return "SIGPROF";
+      case SIGPROF: return "SIGPROF";
 #endif
 #ifdef SIGWINCH
-    case SIGWINCH: return "SIGWINCH";
+      case SIGWINCH: return "SIGWINCH";
 #endif
 #ifdef SIGUSR1
-    case SIGUSR1: return "SIGUSR1";
+      case SIGUSR1: return "SIGUSR1";
 #endif
 #ifdef SIGUSR2
-    case SIGUSR2: return "SIGUSR2";
+      case SIGUSR2: return "SIGUSR2";
 #endif
 #ifdef SIGEMT
-    case SIGEMT: return "SIGEMT";
+      case SIGEMT: return "SIGEMT";
 #endif
 #ifdef SIGSYS
-    case SIGSYS: return "SIGSYS";
+      case SIGSYS: return "SIGSYS";
 #endif
 #ifdef SIGSTKFLT
-    case SIGSTKFLT: return "SIGSTKFLT";
+      case SIGSTKFLT: return "SIGSTKFLT";
 #endif
 #ifdef SIGINFO
-    case SIGINFO: return "SIGINFO";
+      case SIGINFO: return "SIGINFO";
 #endif
 #ifdef SIGPWR
-    case SIGPWR: return "SIGPWR";
+      case SIGPWR: return "SIGPWR";
 #endif
 #ifdef SIGLOST
-    case SIGLOST: return "SIGLOST";
+      case SIGLOST: return "SIGLOST";
 #endif
-  }
+    }
 
   static char s[200];
-  sprintf (s, "signal %d", signal);
+  snprintf (s, sizeof (s), "signal %d", signal);
   return s;
 }
